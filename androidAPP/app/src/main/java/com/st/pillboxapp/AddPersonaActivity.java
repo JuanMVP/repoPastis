53f6b3477bf09c5d6c7a215ef2a0b9f39baca8fe
pepe_gm <pepemoron747@gmail.com>
2019-01-24 08:59:15 +0100

    añadido addPersona

---
+++ androidAPP/app/src/main/java/com/st/pillboxapp/AddPersonaActivity.java
@@ -0,0 +1,13 @@
+package com.st.pillboxapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPersonaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_persona);
+    }
+}
