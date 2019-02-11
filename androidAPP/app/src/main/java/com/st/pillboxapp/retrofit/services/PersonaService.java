50e24d1e48f4becebf4d2cd625342cab4aa4610b
Jose David <josedavid97.pd@gmail.com>
2019-02-11 19:48:19 +0100

    Cambios varios

--- androidAPP/app/src/main/java/com/st/pillboxapp/retrofit/services/PersonaService.java
+++ androidAPP/app/src/main/java/com/st/pillboxapp/retrofit/services/PersonaService.java
@@ -12,6 +12,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PersonaService {
 
