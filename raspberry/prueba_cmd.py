68de48d8f495844fb20a90e96cc7974b08636733
pepe747 <pepemoron747@gmail.com>
2019-02-14 10:58:37 +0100

    codee

---
+++ raspberry/prueba_cmd.py
@@ -0,0 +1,5 @@
+import os
+l=0
+#l=os.popen('sudo python prueba_Bluetooth.py').read()
+os.popen('python prueba_sonido.py')
+print(l)
--- raspberry/prueba_sonido.py
+++ raspberry/prueba_sonido.py
@@ -1,12 +1,16 @@
 from gpiozero import Buzzer
 from time import sleep
 
-buzzer = Buzzer(26)
-while True:
-    buzzer.on()
-    sleep(1)
-    buzzer.off()
-    sleep(1)
+buzzer = Buzzer(13)
+#while True:
+#    buzzer.on()
+#    sleep(1)
+#    buzzer.off()
+#    sleep(1)
 
-while True:
-    buzzer.beep()
+buzzer.on()
+sleep(1)
+buzzer.off()
+sleep(1)
+#while True:
+#    buzzer.beep()
