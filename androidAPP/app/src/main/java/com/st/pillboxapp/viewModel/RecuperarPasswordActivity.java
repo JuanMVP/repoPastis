f2258082e91d028af3ac64ba616abbf8347ed2f1
mariojr2609 <mariojr2609@gmail.com>
2019-02-15 14:19:10 +0100

    ya

---
+++ androidAPP/app/src/main/java/com/st/pillboxapp/viewModel/RecuperarPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.st.pillboxapp.viewModel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.st.pillboxapp.R;
+
+public class RecuperarPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_password);
+    }
+}
